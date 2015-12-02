b695c1aff9a4882e09da08df74b8f6c819945c6a
Sam Kreter <samkreter@gmail.com>
2015-12-02 11:03:03 -0600

    added saminal class

---
+++ include/saminal.hpp
@@ -0,0 +1,21 @@
+#ifndef SAMINAL_HPP__
+#define SAMINAL_HPP__
+
+class Saminal{
+
+private:
+    const std::string basic_cmd[4] = {"ls","cd","pwd","cat"};
+    int ls();
+    int cd(std::string* args);
+    int pwd();
+    int cat();
+    std::string* parse_args(std::string* args);
+    int exec_basic(std::string* args);
+    int exec_added(std::string* args);
+
+public:
+    void Run();
+};
+
+
+#endif
--- src/saminal.cpp
+++ src/saminal.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <string>
 #include <boost/filesystem.hpp>
+#include <boost/thread.hpp>
+
+#include "../include/saminal.hpp"
+
+namespace fs = boost::filesystem;
+using namespace std;
 
 int main(int argc, char** argv){
 
-    std::cout<<"heY"<<std::endl;
+
+    // fs::path full_path( fs::initial_path<fs::path>() );
+    // cout<<full_path<<endl;
+    // cout<<full_path.filename()<<endl;
+    // cout<<fs::is_directory(full_path.parent_path())<<endl;
+    Saminal s;
+
 
     return 0;
 }
+
+
